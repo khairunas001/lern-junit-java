ef19be8663a8c296ba5ffb80a1a2402baf96ce12
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-05-23 23:20:25 +0700

    23-5-2025

--- src/test/java/learn/junit/anas/javaunittest/RandomCalculatorTest.java
+++ src/test/java/learn/junit/anas/javaunittest/RandomCalculatorTest.java
@@ -4,24 +4,28 @@ import learn.junit.anas.javaunittest.resolver.RandomParameterResolver;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 //@Extensions({
 //        @ExtendWith(RandomParameterResolver.class)
 //})
-public class RandomCalculatorTest extends AbstractCalculatorTest{
+public class RandomCalculatorTest extends AbstractCalculatorTest {
 
     //  private Calculator calculator = new Calculator();
     @Test
-    void testRandom(Random random, TestInfo info){
+    void testRandom(Random random, TestInfo info) {
         var a = random.nextInt();
         var b = random.nextInt();
 
         var result = calculator.add(a, b);
         var expected = a + b;
 
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
 
     }
 
@@ -30,14 +34,14 @@ public class RandomCalculatorTest extends AbstractCalculatorTest{
             value = 10,
             name = "{displayName} ke -> ( {currentRepetition} dari {totalRepetitions} )"
     )
-    void testRandomRepeat(Random random, TestInfo info){
+    void testRandomRepeat(Random random, TestInfo info) {
         var a = random.nextInt();
         var b = random.nextInt();
 
         var result = calculator.add(a, b);
         var expected = a + b;
 
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
 
     }
 
@@ -47,18 +51,44 @@ public class RandomCalculatorTest extends AbstractCalculatorTest{
             value = 10,
             name = "{displayName} ke -> ( {currentRepetition} dari {totalRepetitions} )"
     )
-    void testRandomRepeatInfo(Random random, TestInfo info, RepetitionInfo repetitionInfo){
-        System.out.println(info.getDisplayName()+" ke -> "+ repetitionInfo.getCurrentRepetition() +" dari "+ repetitionInfo.getTotalRepetitions());
+    void testRandomRepeatInfo(Random random, TestInfo info, RepetitionInfo repetitionInfo) {
+        System.out.println(info.getDisplayName() + " ke -> " + repetitionInfo.getCurrentRepetition() + " dari " + repetitionInfo.getTotalRepetitions());
         var a = random.nextInt();
         var b = random.nextInt();
 
         var result = calculator.add(a, b);
         var expected = a + b;
 
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected, result);
 
     }
 
     //https://www.youtube.com/watch?v=0jreaBRIOTo&list=PL-CtdCApEFH-p_Q2GyK4K3ORoAT0Yt7CX&index=32
     // sampe menit 2.00.00
+
+
+    // jika menggunakan cara value source
+    @DisplayName("Test Calculator With Parameter")
+    @ParameterizedTest(name = "{displayName} with data {0}")
+    @ValueSource(ints = {1,2,3,4,65,8,9,22,334,4,})
+    void testWithParameter(int value){
+        var expected = value + value;
+        var result = calculator.add(value,value);
+        Assertions.assertEquals(expected,result);
+    }
+
+
+    // jika menggunakan cara method source harus buat method dulu sebelum melakukan test
+    public static List<Integer> parameterSource(){
+        return List.of(1,3,4,66,777,7,4);
+    }
+
+    @DisplayName("Test Calculator With Parameter")
+    @ParameterizedTest(name = "{displayName} with data {0}")
+    @MethodSource("parameterSource")
+    void testWithMethodSource(Integer value){
+        var expected = value + value;
+        var result = calculator.add(value,value);
+        Assertions.assertEquals(expected,result);
+    }
 }
---
+++ src/test/java/learn/junit/anas/javaunittest/SlowTest.java
@@ -0,0 +1,37 @@
+package learn.junit.anas.javaunittest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.util.concurrent.TimeUnit;
+
+
+// @Execution(value = ExecutionMode.CONCURRENT) digunakan untuk menjalankan unit test secara parallel
+@Execution(value = ExecutionMode.CONCURRENT)
+public class SlowTest {
+
+    @Test
+    // digunaaakan untuk menentukan timeout dari sebuah uni test
+    // jika lebih dari waktu yang sudah ditentukan maka unit test akan dianggap gagal
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow1() throws InterruptedException {
+        Thread.sleep(1_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow2() throws InterruptedException {
+        Thread.sleep(3_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow3() throws InterruptedException {
+        Thread.sleep(4_000);
+    }
+
+    //Pengenalan Mocking 2.15.00
+    //https://www.youtube.com/watch?v=0jreaBRIOTo&list=PL-CtdCApEFH-p_Q2GyK4K3ORoAT0Yt7CX&index=34
+}
